7af7cc4d06e597f1eb1628d1f24eabc97fddd351
olemor2706 <olemor@mail.ru>
2018-09-27 21:02:16 +0600

    222

--- server.js
+++ server.js
@@ -10,12 +10,34 @@ const port = 8080;
 // response - ответсное сообщение
 function handle (request, response){
 	console.log("message received");
+	console.log(request);
 	// состояние  http
 	response.statusCode = 200;
-	// Установка заголовка ответа
+	// Установка заголовка ответа 
+	//text/plain - обычный текст, который браузер не обрабатывает
+	//text/html - браузер обрабатывает как html
 	response.setHeader('Content-type', 'text/plain');
+	// Формируем текст ответа
+	var content = '<!DOCTYPE html>' +
+		'<html>' +
+		'<head>' +
+		'<meta charset="utf-8"/>' +
+		'</head>' +
+		'<body>';
+	if (request.url.includes("file2")){
+		// содержимое файла 2 response.end("task1");
+	} else if (request.url.includes("file2")){
+	//
+	}else {
+		// содержимое файла 1
+		content = content + "<a href = 'file2.html'> file2</a>";
+		
+	
 	// Отправляем ответ клиенту
-	response.end('It works!');
+	}
+	content = content + '</body>' +
+		'</html>';
+	response.end(content);
 }
 
 const server = http.createServer (handle);
