8855c34dc7384f7b7c25a2c41eff36d650b9cb82
Oleg <olemor@mail.ru>
2018-10-03 15:00:05 +0600

    Homework

--- server.js
+++ server.js
@@ -7,16 +7,16 @@ const port = 8080;
 
 // функция для обработки сообщений
 // request - полуяенное сообщение
-// response - ответсное сообщение
+// response - ответное сообщение
 function handle (request, response){
 	console.log("message received");
-	console.log(request);
+	//console.log(request);
 	// состояние  http
 	response.statusCode = 200;
 	// Установка заголовка ответа 
 	//text/plain - обычный текст, который браузер не обрабатывает
 	//text/html - браузер обрабатывает как html
-	response.setHeader('Content-type', 'text/plain');
+	response.setHeader('Content-type', 'text/html');
 	// Формируем текст ответа
 	var content = '<!DOCTYPE html>' +
 		'<html>' +
@@ -24,6 +24,26 @@ function handle (request, response){
 		'<meta charset="utf-8"/>' +
 		'</head>' +
 		'<body>';
+	while (true){
+		if (request.url.includes("file2")){
+			content = content + "<a href = 'file3.html'> file3</a>" +
+				"<a href = 'file4.html'> file4</a>";
+			break;
+		} 
+		if (request.url.includes("file3")){
+			content = content + "<a href = 'file4.html'> file4</a>";
+			break;
+		} 
+		if (request.url.includes("file4")){
+			content = content + "<a href = 'file1.html'> file1</a>";
+			break;
+		} 
+		content = content + "<a href = 'file2.html'> file2</a> " +
+			"<a href = 'file3.html'> file3</a> " +
+			"<a href = 'file4.html'> file4</a>";
+		break;
+	}
+	/*
 	if (request.url.includes("file2")){
 		// содержимое файла 2 response.end("task1");
 	} else if (request.url.includes("file2")){
@@ -34,7 +54,8 @@ function handle (request, response){
 		
 	
 	// Отправляем ответ клиенту
-	}
+	}*/
+	
 	content = content + '</body>' +
 		'</html>';
 	response.end(content);
